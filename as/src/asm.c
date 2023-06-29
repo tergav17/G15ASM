79969854910a83f9c3eb97be5095d2cd803c1f21
tergav17 <gctersteeg@gmail.com>
2023-06-29 17:38:29 -0500

    Stole some service routines from TRASM

--- as/src/asm.c
+++ as/src/asm.c
@@ -1,3 +1,89 @@
+/*
+ * asm.c 
+ *
+ * assembler guts
+ */
+ #include "asm.h"
+#include "sio.h"
+
+char token_buf[TOKEN_BUF_SIZE];
+
+/*
+ * skips past all of the white space to a token
+ */
+void asm_wskip()
+{
+	char comment;
+	
+	comment = 0;
+	while ((sio_peek() <= ' ' || sio_peek() == ';' || comment) && sio_peek() != '\n' && sio_peek() != -1)
+		if (sio_next() == ';') comment = 1;
+}
+
+/*
+ * tests if a character is a alpha (aA - zZ or underscore)
+ *
+ * in = character to test
+ * returns true (1) or false (0)
+ */
+char asm_alpha(char in)
+{
+	return (in >= 'A' && in <= 'Z') || (in >= 'a' && in <= 'z') || in == '_';
+}
+
+/*
+ * tests if a character is a alpha
+ *
+ * in = character to test
+ * returns true (1) or false (0)
+ */
+char asm_num(char in)
+{
+	return (in >= '0' && in <= '9');
+}
+
+/*
+ * reads the next token in from the source, buffers if needed, and returns type
+ * white space will by cycled past, both in front and behind the token
+ */
+char asm_token_read() 
+{
+	char c, out;
+	int i;
+
+	// skip all leading white space
+	asm_wskip();
+	
+	// peek and check type
+	out = c = sio_peek();
+	if (asm_alpha(c))
+		out = 'a';
+	else if (asm_num(c))
+		out = '0';
+	
+	if (out == 'a' || out == '0') {
+			// scan in the buffer if needed
+		i = 0;
+		while (asm_num(c) || asm_alpha(c)) {
+			if (i < TOKEN_BUF_SIZE - 1)
+				token_buf[i++] = c;
+			
+			sio_next();
+			c = sio_peek();
+		}
+		token_buf[i] = 0;
+	} else {
+		sio_next();
+	}
+	
+	// correct for new lines
+	if (out == '\n') out = 'n';
+	
+	// skip more whitespace 
+	asm_wskip();
+	
+	return out;
+}
 
 void asm_assemble(char flagv) {
 	
--- as/src/asm.h
+++ as/src/asm.h
@@ -1,3 +1,13 @@
+#ifndef ASM_H
+#define ASM_H
+
+/* includes */
+#include <stdint.h>
+
+/* defines */
+#define TOKEN_BUF_SIZE 19
 
 /* prototyes */
 void asm_assemble(char flagv);
+
+#endif
